eabf77259796baab6b6e716f7feb6dc8440d36c3
Husny Ahamed M.G <mgha123@gmail.com>
2024-01-26 17:00:07 +0100

    FE landing page is done, looks nice and easy. Backend is WIP Infra is WIP

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # e-commerce-cloud
+
+1. Lambda functions needed
+
+| Lambda Function Name  | Description                                                  |
+| --------------------- | ------------------------------------------------------------ |
+| UserRegistration      | Handles user sign-up and registration logic.                 |
+| UserAuthentication    | Manages user login and token generation.                     |
+| UserProfileManagement | Allows users to view and edit their profile information.     |
+| ProductCatalogueQuery | Retrieves the list of products for the catalogue view.       |
+| ProductDetails        | Fetches detailed information for a specific product.         |
+| ShoppingCartUpdate    | Adds, removes, and updates items in a user's shopping cart.  |
+| OrderProcessing       | Processes user orders, including payment and confirmation.   |
+| InventoryManagement   | Updates and tracks inventory for available products.         |
+| OrderHistory          | Retrieves the order history for a user.                      |
+| UserAuthorization     | Checks if the user is authorized to perform certain actions. |
--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,16 +1,28 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 
 export class InfrastructureStack extends cdk.Stack {
+  public readonly usersTable: dynamodb.Table;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, "MyEcommerceWebsiteBucket", {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Define the DynamoDB table for Users
+    this.usersTable = new dynamodb.Table(this, "UsersTable", {
+      partitionKey: { name: 'userId', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'email', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST, // This mode is cost-effective for low-traffic patterns
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Only for dev/test environments 
+    })
   }
+
+
 }
